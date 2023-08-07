68defe477ddf443f8110c482d697f51d3b0c63d4
Facu00H <faisher00@gmail.com>
2023-08-07 12:32:30 -0300

    feat: add features and header/footer layout

---
+++ src/components/organism/featuresData.ts
@@ -0,0 +1,33 @@
+interface IFeatureData {
+  iconName: string;
+  featureTitle: string;
+  text: string;
+};
+
+export const featuresData: IFeatureData[] = [
+  {
+    iconName: "carbon:chart-cluster-bar",
+    featureTitle: "Deliver instant answers",
+    text: "An all-in-one customer service platform that helps you balance everything your customers need to be happy."
+  },
+  {
+    iconName: "carbon:lightning",
+    featureTitle: "Manage your team with reports",
+    text: "Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks."
+  },
+  {
+    iconName: "carbon:face-satisfied",
+    featureTitle: "Connect with customers",
+    text: "Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion."
+  },
+  {
+    iconName: "carbon:concept",
+    featureTitle: "Connect the tools you already use",
+    text: "Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools."
+  },
+  {
+    iconName: "carbon:chat",
+    featureTitle: "Our people make the difference",
+    text: "We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help."
+  },
+]
