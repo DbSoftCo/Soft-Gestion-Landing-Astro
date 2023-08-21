b2c84eb53532ef2e4b9496e4f62d771d6a3aded9
Facu00H <faisher00@gmail.com>
2023-08-21 16:07:11 -0300

    ref: change language text to spanish

--- src/components/organism/featuresData.ts
+++ src/components/organism/featuresData.ts
@@ -7,28 +7,38 @@ interface IFeatureData {
 export const featuresData1: IFeatureData[] = [
   {
     iconName: "carbon:chart-cluster-bar",
-    featureTitle: "Deliver instant answers",
-    text: "An all-in-one customer service platform that helps you balance everything your customers need to be happy."
+    featureTitle: "Modulo de Obras",
+    text: "Nuestro Módulo de Obras transforma la administración de proyectos. Desde un vistazo, accede a datos vitales como el estado de obras, motivos de cancelación y áreas de concentración. Controla, asigna supervisores y edita proyectos con facilidad. Además, genera informes personalizados para mantener el control total de tu construcción."
   },
   {
     iconName: "carbon:lightning",
-    featureTitle: "Manage your team with reports",
-    text: "Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks."
+    featureTitle: "Gestion del personal de obra",
+    text: "An all-in-one customer service platform that helps you balance everything your customers need to be happy."
   },
   {
     iconName: "carbon:face-satisfied",
-    featureTitle: "Connect with customers",
-    text: "Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion."
+    featureTitle: "Dashboard operativo",
+    text: "Un Dashboard Operativo es tu herramienta esencial para el control efectivo. Proporciona una visión panorámica y en tiempo real de tus proyectos, con datos clave como estados, avances y métricas relevantes. Facilita la toma de decisiones informadas y agiliza la gestión, permitiéndote supervisar y optimizar cada aspecto de tu operación con confianza."
   },
   {
     iconName: "carbon:concept",
-    featureTitle: "Connect the tools you already use",
+    featureTitle: "Modulo de RRHH",
+    text: "Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion."
+  },
+  {
+    iconName: "carbon:chat",
+    featureTitle: "Modulo de cuentas corrientes",
     text: "Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools."
   },
   {
     iconName: "carbon:chat",
-    featureTitle: "Our people make the difference",
+    featureTitle: "Facturacion electronica",
     text: "We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help."
+  },
+  {
+    iconName: "carbon:chat",
+    featureTitle: "Stock e inventario",
+    text: "Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks."
   },
 ]
 
