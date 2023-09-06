055ae9573b473bb33895019118be7fc198a5a4bc
Facu00H <faisher00@gmail.com>
2023-09-06 13:19:56 -0300

    push

--- src/components/organism/accordeonInfo.ts
+++ src/components/organism/accordeonInfo.ts
@@ -5,27 +5,27 @@ export interface IaccordeonData {
 
 export const accordeonData: IaccordeonData[] = [
   {
-    title: "Puedo tener una prueba gratuita?",
+    title: "¿Puedo tener una prueba gratuita?",
     text: "Sí, podes solicitar tu demo gratuita. Si lo desea, le brindaremos una llamada de incorporación gratuita y personalizada de 30 minutos para que pueda comenzar a funcionar lo antes posible.",
   },
   {
-    title: "Puedo contratar modulos especificos?",
-    text: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    title: "¿Puedo contratar modulos especificos?",
+    text: "Sí, en nuestra plataforma ofrecemos la flexibilidad de contratar módulos específicos según tus necesidades. Esto te permite personalizar tu experiencia y adaptar nuestro servicio a los requerimientos particulares de tu negocio o proyecto.",
   },
   {
-    title: "Puedo cancelar mi suscripcion al servicio?",
-    text: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    title: "¿Puedo cancelar mi suscripcion al servicio?",
+    text: "Por supuesto, entendemos que las necesidades pueden cambiar con el tiempo. Puedes cancelar tu suscripción en cualquier momento sin ninguna penalización. Simplemente accede a tu cuenta y sigue los pasos sencillos para gestionar tu suscripción.",
   },
   {
-    title: "Ofrecen capacitaciones?",
-    text: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    title: "¿Ofrecen capacitaciones?",
+    text: "Sí, estamos comprometidos con el éxito de nuestros usuarios. Ofrecemos capacitaciones en línea y recursos de aprendizaje para ayudarte a aprovechar al máximo nuestra plataforma. Nuestro equipo de soporte también está disponible para responder tus preguntas y brindarte asistencia.",
   },
   {
-    title: "La app funciona en IOS?",
-    text: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    title: "¿La app funciona en IOS?",
+    text: "¡Absolutamente! Nuestra aplicación es compatible con iOS y Android, lo que te permite acceder a nuestros servicios desde una amplia variedad de dispositivos. Puedes descargar la aplicación desde la App Store de Apple y comenzar a usarla de inmediato.",
   },
   {
-    title: "Como puedo abonar el servicio?",
-    text: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    title: "¿Como puedo abonar el servicio?",
+    text: "Hacer el pago es fácil y conveniente. Aceptamos varios métodos de pago, incluyendo tarjetas de crédito y débito, así como opciones de pago en línea. Puedes configurar tu método de pago preferido en tu cuenta y realizar transacciones de forma segura.",
   },
 ];
