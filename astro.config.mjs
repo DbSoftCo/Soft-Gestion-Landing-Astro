2d4e0b42e8da97de98ccd779678f44da126530e7
Facu00H <faisher00@gmail.com>
2024-01-31 23:32:15 -0300

    feat: upgrage to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,6 @@ export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
-    imagesConfig: { sizes: [320, 640, 1280] },
     imageService: true,
   })
 });
