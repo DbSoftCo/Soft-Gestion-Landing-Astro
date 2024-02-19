13aefd82a56f26061ae7f786f4f49428c9bff37e
Facu00H <faisher00@gmail.com>
2024-02-18 21:50:37 -0300

    fix brackpoints

--- src/components/organism/FeaturesReact.tsx
+++ src/components/organism/FeaturesReact.tsx
@@ -16,8 +16,8 @@ export default function FeaturesReact() {
   return (
     <section className="bg-purple-200 w-full pt-4 rounded-md h-fit">
       <Tabs defaultValue={ featuresData1[0].featureTitle } className="w-full rounded-none h-fit flex flex-col items-center justify-center gap-2">
-        <ScrollArea className="w-full whitespace-nowrap rounded-none border lg:w-fit lg:m-auto border-none h-[10%]">
-          <TabsList className="flex bg-purple-700 w-full rounded-none lg:rounded-lg lg:w-fit">
+        <ScrollArea className="w-full whitespace-nowrap rounded-none border xl:w-fit xl:m-auto border-none h-[10%]">
+          <TabsList className="flex bg-purple-700 w-full rounded-none xl:rounded-lg lg:w-fit">
             {
               featuresData1.map(({ featureTitle }) => {
                 return (
@@ -33,24 +33,23 @@ export default function FeaturesReact() {
             return (
               <TabsContent key={id + featureTitle} value={ featureTitle }>
                   <div className="flex flex-col-reverse lg:flex-row px-6 gap-8 my-8">
-                    <div className="flex flex-col gap-10 ml-9 lg:w-[50%] h-full">
+                    <div className="flex flex-col gap-7 ml-9 lg:w-[50%] h-full">
                       <h3 className="w-full text-left">No pierdas el control de tus obras de nuevo!</h3>
-                      <span className="text-left text-base font-semibold">{text}</span>
-                      <ul className="flex flex-col items-start gap-1 text-base font-semibold text-left">
+                      <span className="text-left text-xs font-semibold">{text}</span>
+                      <ul className="flex flex-col items-start gap-1 text-xs font-semibold text-left">
                         <li className="text-left">Genera un reporte mensual con el estado de tu obra.</li>
                         <li className="text-left">Gestiona el personal asignado a cada obra.</li>
                         <li className="text-left">Verifica los gastos de tus obras y hacia donde son asignado.</li>
                       </ul>
                     </div>
-                    <div className="mx-2 lg:w-[50%] lg:px-10">
+                    <div className="mx-2 lg:w-[50%] flex flex-col justify-center items-center">
                     <Carousel opts={{
                       dragFree: true,
                       active: true
                     }}
                     plugins={[
                       Autoplay({
-                        Delay: 2500,
-                        stopOnMouseEnter: true
+                        Delay: 2000,
                       }),
                     ]}
                     >
